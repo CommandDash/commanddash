747df99e2c61b7544b4d4ca827590ab817bd022d
Tanmay Munjal <72982935+tanmaymunjal@users.noreply.github.com>
2023-04-26 14:38:56 +0530

    add more descriptive error mesage for openai api erros

--- src/repository/http-utils.ts
+++ src/repository/http-utils.ts
@@ -5,6 +5,10 @@ export async function makeHttpRequest<T>(config: AxiosRequestConfig): Promise<T>
         const response: AxiosResponse<T> = await axios(config);
         return response.data;
     } catch (error) {
-        throw new Error(`Failed to make HTTP request: ${error}`);
+        if (axios.isAxiosError(error)) {
+            throw new Error('OpenAI API threw an error with status code ${error.status}. Please check https://platform.openai.com/docs/guides/error-codes/api-errors for more information.');
+        } else {
+            throw new Error(`Failed to make HTTP request: ${error}`);
+        };
     }
 }
