b085c8a4b91767c83969e8effe9802f0eba2006b
Yatendra Kumar <yatendraroria2001@gmail.com>
2024-01-07 14:40:47 +0530

    fix: tuned prompt

--- src/repository/gemini-repository.ts
+++ src/repository/gemini-repository.ts
@@ -42,7 +42,7 @@ export class GeminiRepository {
         let lastMessage = prompt.pop();
         if (lastMessage && isReferenceAdded) {
             const dartFiles = await this.findClosestDartFiles(lastMessage.parts);
-            lastMessage.parts = "You're a vscode extension copilot, you've complete access to the codebase. I'll provide you with top 5 closest files code as context and your job is to read following workspace code end-to-end and answer the prompt initialised by `@workspace` symbol. If you're unable to find answer for the requested prompt, suggest an alternative solution as a dart expert. Be crisp & crystal clear in your answer. Make sure to provide your thinking process in steps. Here's the code: \n\n" + dartFiles + "\n\n" + lastMessage.parts;
+            lastMessage.parts = "You've complete access to the codebase. I'll provide you with top 5 closest files code as context and your job is to read following workspace code end-to-end and answer the prompt initialised by `@workspace` symbol. If you're unable to find answer for the requested prompt, suggest an alternative solution as a dart expert. Be crisp & crystal clear in your answer. Make sure to provide your thinking process in steps along with file name, path & code. Here's the code: \n\n" + dartFiles + "\n\n" + lastMessage.parts;
         }
         const chat = this.genAI.getGenerativeModel({ model: "gemini-pro", generationConfig: { temperature: 0.0, topP: 0.2 } }).startChat(
             {
