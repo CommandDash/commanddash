5c7c38fbafe1d680d134337a3e50194dc2cfcc35
Samyak Jain <56000318+samyakkkk@users.noreply.github.com>
2024-04-22 21:19:27 +0530

    (fix): remove unncessary comment

--- vscode/src/utilities/commanddash-integration/dart-cli-client.ts
+++ vscode/src/utilities/commanddash-integration/dart-cli-client.ts
@@ -118,7 +118,6 @@ export class DartCLIClient {
 
   public connect() {
     this.proc = child_process.spawn(this.executablePath, ['process']);
-    // this.proc = child_process.spawn('dart', ['run', '/Users/samyak/Documents/commanddash/commanddash/commanddash/bin/commanddash.dart', 'process']);
 
     let buffer = '';
 
