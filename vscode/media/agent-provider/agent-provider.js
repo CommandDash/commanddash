f0bde13df6c6c08fdc708666e5540e1e13d51435
wadhia-yash <aarushwadhia@gmail.com>
2024-05-01 17:12:35 +0530

    fix(Agent's slug): Added testing field in the agents commands

--- vscode/media/agent-provider/agent-provider.js
+++ vscode/media/agent-provider/agent-provider.js
@@ -6,7 +6,7 @@ class AgentProvider {
     for (const item of this.json) {
       for (const command of item.supported_commands) {
         if (command.slug === inputString) {
-          return JSON.parse(JSON.stringify(command));
+          return JSON.parse(JSON.stringify({...command, testing: item.testing}));
         }
       }
     }
