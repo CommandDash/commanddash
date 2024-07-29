12c03797e8a48d7f5f58b7e2dc19d7afd6e8cc1b
wadhia-yash <aarushwadhia@gmail.com>
2024-07-29 11:06:19 +0530

    fix(chatinput): Chatinput changes the height when pressed enter

--- web/src/lib/components/chat/ChatInput.svelte
+++ web/src/lib/components/chat/ChatInput.svelte
@@ -1,65 +1,64 @@
 <script lang="ts">
-    import { isDesktop } from "$lib/utils/isDesktop";
-    import { onMount, createEventDispatcher } from "svelte";
+	import { isDesktop } from "$lib/utils/isDesktop";
+	import { createEventDispatcher, onMount } from "svelte";
 
-    export let value: string = "";
-    export let placeholder: string = "";
-    export let minRows = 1;
-    export let maxRows: null | number = null;
-    export let disabled: boolean = false;
+	export let value = "";
+	export let minRows = 1;
+	export let maxRows: null | number = null;
+	export let placeholder = "";
+	export let disabled = false;
 
-    let textareaElement: HTMLTextAreaElement;
+	let textareaElement: HTMLTextAreaElement;
 
-    const dispatch = createEventDispatcher<{ submit: void }>();
+	const dispatch = createEventDispatcher<{ submit: void }>();
 
-    $: minHeight = `${1 + minRows * 1.5}em`;
-    $: maxHeight = maxRows ? `${1 + maxRows * 1.5}em` : `auto`;
+	$: minHeight = `${1 + minRows * 1.5}em`;
+	$: maxHeight = maxRows ? `${1 + maxRows * 1.5}em` : `auto`;
 
-    function handleKeydown(event: KeyboardEvent) {
-        //submit on enter
-        if (event.key === "Enter" && !event.shiftKey) {
-            event.preventDefault();
+	function handleKeydown(event: KeyboardEvent) {
+		if (event.key === "Enter" && !event.shiftKey) {
+			event.preventDefault();
+			textareaElement.blur();
+			if (isDesktop(window)) {
+				textareaElement.focus();
+			}
+			dispatch("submit");
+		}
+	}
 
-            textareaElement.blur();
-
-            if (isDesktop(window)) {
-                textareaElement.focus();
-            }
-
-            dispatch("submit");
-        }
-    }
-
-    onMount(() => {
-        if (isDesktop(window)) {
-            textareaElement.focus();
-        }
-    });
+	onMount(() => {
+		if (isDesktop(window)) {
+			textareaElement.focus();
+		}
+	});
 </script>
 
 <div class="relative min-w-0 flex-1" on:paste>
-    <pre
-        class="scrollbar-custom invisible overflow-x-hidden overflow-y-scroll whitespace-pre-wrap break-words p-3"
-        aria-hidden="true"
-        style="min-height: {minHeight}; max-height: {maxHeight}"></pre>
-    <textarea
-        enterkeyhint="send"
-        tabindex="0"
-        rows="1"
-        bind:value
-        bind:this={textareaElement}
-        class="scrollbar-custom absolute top-0 m-0 h-full w-full resize-none scroll-p-3 overflow-x-hidden overflow-y-scroll border-0 bg-transparent p-3 outline-none focus:ring-0 focus-visible:ring-0"
-        {placeholder}
-        {disabled}
-        on:keydown={handleKeydown}
-    />
+	<pre
+		class="scrollbar-custom invisible overflow-x-hidden overflow-y-scroll whitespace-pre-wrap break-words p-3"
+		aria-hidden="true"
+		style="min-height: {minHeight}; max-height: {maxHeight}">{(value || " ") + "\n"}</pre>
+
+	<textarea
+		enterkeyhint="send"
+		tabindex="0"
+		rows="1"
+		class="scrollbar-custom absolute top-0 m-0 h-full w-full resize-none scroll-p-3 overflow-x-hidden overflow-y-scroll border-0 bg-transparent p-3 outline-none focus:ring-0 focus-visible:ring-0"
+		class:text-gray-400={disabled}
+		bind:value
+		bind:this={textareaElement}
+		{disabled}
+		on:keydown={handleKeydown}
+		on:beforeinput
+		{placeholder}
+	/>
 </div>
 
 <style>
-    pre,
-    textarea {
-        font-family: inherit;
-        box-sizing: border-box;
-        line-height: 1.5;
-    }
+	pre,
+	textarea {
+		font-family: inherit;
+		box-sizing: border-box;
+		line-height: 1.5;
+	}
 </style>
