4ed61b9f74045ac057eeb1594d9329cea6ad8a60
Samyak Jain <56000318+samyakkkk@users.noreply.github.com>
2024-08-10 18:30:56 +0530

    (feat): handle local env configuration

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import Icons from "unplugin-icons/vite";
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file only in development
+// Github actions work without dotenv
+if (process.env.NODE_ENV !== 'production') {
+	dotenv.config();
+  }
 
 export default defineConfig({
 	define: {
