81fae5b14fd7c6159a8abc5c253c8a2f1d81f99d
Samyak Jain <56000318+samyakkkk@users.noreply.github.com>
2024-08-12 12:50:58 +0530

    (feat): copy badge button added

---
+++ web/src/lib/utils/copyToClipboard.ts
@@ -0,0 +1,9 @@
+// src/lib/utils/copyToClipboard.ts
+export function copyToClipboard(text: string) {
+    const textarea = document.createElement("textarea");
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+}
