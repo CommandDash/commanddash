42ef9c6c083f5e6f4410259795ac2d731e4a53f1
wadhia-yash <aarushwadhia@gmail.com>
2024-08-15 07:49:10 +0530

    fix(Questionnaire): Removed grid with two columns and made with one column

--- vscode/media/questionnaire/questionnaire.js
+++ vscode/media/questionnaire/questionnaire.js
@@ -13,10 +13,10 @@ class Questionnaire {
         this.json.forEach(({ id, message, onclick, icon, isGradient }) => {
             container = document.createElement("div");
             container.id = id;
-            container.classList.add("h-32", "max-w-full", "p-1", "cursor-pointer");
+            container.classList.add("max-w-full", "p-1", "cursor-pointer");
 
             const cardContainer = document.createElement("div");
-            cardContainer.classList.add("h-32", "w-full", "rounded-lg", isGradient ? "questionnaire-card-gradient" : "questionnaire-card", "py-2", "px-3", "relative");
+            cardContainer.classList.add("w-full", "rounded-lg", isGradient ? "questionnaire-card-gradient" : "questionnaire-card", "py-5", "px-3", "relative", "text-center");
             cardContainer.addEventListener("click", () => onclick(this.ref));
 
             const _message = document.createElement("span");
@@ -25,12 +25,11 @@ class Questionnaire {
             _message.classList.add("font-bold");
             _message.textContent = message;
 
-            const iconContainer = document.createElement("div");
-            iconContainer.classList.add("absolute", "bottom-1", "right-1", "border", "border-black", "rounded-full", "p-1", "bg-white");
-            iconContainer.innerHTML = icon;
+            // const iconContainer = document.createElement("div");
+            // iconContainer.classList.add("absolute", "bottom-1", "right-1", "border", "border-black", "rounded-full", "p-1", "bg-white");
+            // iconContainer.innerHTML = icon;
 
             cardContainer.appendChild(_message);
-            cardContainer.appendChild(iconContainer);
             container.appendChild(cardContainer);
             grid.appendChild(container);
         });
